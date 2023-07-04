a588ff571047b12fe9fc213916162ea0d4fe7f29
zeeshan gulzar <xeeshansubs@gmail.com>
2023-07-04 13:38:16 +0500

    scaffold posts

---
+++ db/migrate/20230704083329_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
