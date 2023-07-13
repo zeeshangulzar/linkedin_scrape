3afaa635ec537a78812c8bc68c41978fa22e41ca
zeeshan gulzar <xeeshansubs@gmail.com>
2023-07-13 16:52:53 +0500

    scrape 100 or above feed from linkedin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,28 +28,39 @@ class PostsController < ApplicationController
       browser.input(name: 'session_key').send_keys(params[:post][:username], :return)
       browser.input(name: 'session_password').send_keys(params[:post][:password], :return)
       sleep(1.minutes)
-      browser.scroll.from(8, 750).by(0, 750)
-      page = browser.html
+
+      pages = []
+      (1..20).each do |num|
+        if pages.present?
+          sleep 2
+          browser.scroll.from(1, 750).by(0, 10000)
+          pages += browser.html
+          browser.button(value: 'See new posts').click
+        else
+          sleep 1
+          pages = browser.html
+        end
+      end
     end
 
-    data=Nokogiri::HTML.parse(page)
+    data=Nokogiri::HTML.parse(pages)
     temp = []
-    cards = data.css(".scaffold-layout__main")
+    total_pages = data.css(".scaffold-layout__main")
+    total_pages.each do |cards|
+      cards.css(".feed-shared-update-v2").each do |card|
+        @post = Post.new(post_params)
+        @title = card&.css(".update-components-actor__title")&.css(".update-components-actor__name span")&.first&.text
+        @content = card.css(".update-components-text").css(".break-words span").first&.text
+        @image = card.css(".ivm-image-view-model").css(".ivm-view-attr__img--centered").last.attr("src")
+        @video = card.css(".media-player").css("video").attr("src")&.value
+        @iframe = card.css("iframe").attr("src")&.value
 
-    cards.css(".feed-shared-update-v2").each do |card|
-      @post = Post.new(post_params)
-      @title = card&.css(".update-components-actor__title")&.css(".update-components-actor__name span")&.first&.text
-      @content = card.css(".update-components-text").css(".break-words span").first&.text
-      @image = card.css(".ivm-image-view-model").css(".ivm-view-attr__img--centered").last.attr("src")
-      @video = card.css(".media-player").css("video").attr("src")&.value
-      @iframe = card.css("iframe").attr("src")&.value
-
-      @post.title = @title if @title.present?
-      @post.content = @content if @content.present?
-      @post.image = @image if @image.present?
-      @post.name = @video if @video.present?
-      @post.image = browser.iframe(src: @iframe).div.img.src if @iframe.present?
-      temp << @post.save
+        @post.title = @title if @title.present?
+        @post.content = @content if @content.present?
+        @post.image = @image if @image.present?
+        @post.name = @video if @video.present?
+        temp << @post.save
+      end
     end
 
     respond_to do |format|
